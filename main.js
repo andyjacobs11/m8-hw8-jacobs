254ab4b9c44262839e633d37a7c662d14e291d22
Andrew-Stephen-Jacobs <andrewjacobs@ufl.edu>
2020-10-27 21:30:02 -0400

    Attempt to fix hover js

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
-var subMenu = document.getElementsByClassName('sub-menu');
-console.log(subMenu);
-
-function toggleSubmenu() {
-    for( i = 0; i < subMenu.length; i++ ){
-         dropdowns[i].classList.toggle('sub-menu');
-     }
-
-    // this.children[1].classList.toggle('show');
-}
-
-for( i = 0; i < navLinks.length; i ++){
-    navLinks[i].addEventListener("mouseenter", toggleDropdown)
-    navLinks[i].addEventListener("mouseleave", toggleDropdown)
+//Hover submenu
+document.getElementsByClassName("main-menu-labels").addEventListener("mouseover", event => {
+    document.getElementsByClassName("sub-menu")style.display = "flex"
 }
