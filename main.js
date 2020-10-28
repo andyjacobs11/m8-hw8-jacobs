48b72e9060feddc88bcfd2c55877158a352597ec
Andrew-Stephen-Jacobs <andrewjacobs@ufl.edu>
2020-10-27 21:16:51 -0400

    Add js & hide submenu

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-// It is always helpful to use comments in your code!
+var subMenu = document.getElementsByClassName('sub-menu');
+console.log(subMenu);
+
+function toggleSubmenu() {
+    for( i = 0; i < subMenu.length; i++ ){
+         dropdowns[i].classList.toggle('sub-menu');
+     }
+
+    // this.children[1].classList.toggle('show');
+}
+
+for( i = 0; i < navLinks.length; i ++){
+    navLinks[i].addEventListener("mouseenter", toggleDropdown)
+    navLinks[i].addEventListener("mouseleave", toggleDropdown)
+}
